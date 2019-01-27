72334a928c2c71ecb1499e404950ca8331f51bd0
SiyanaTodorova <47076923+SiyanaTodorova@users.noreply.github.com>
2019-01-27 13:50:54 +0200

    Create BonusScore.java

---
+++ 3. ConditionalStatements/Exercise/src/BonusScore.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BonusScore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int points = Integer.parseInt(scanner.nextLine());
+        double bonus = 0;
+        if (points <= 100) {
+            bonus = 5;
+        } else if (points <= 1000) {
+            bonus = points * 20 / 100.0;
+        } else {
+            bonus = points * 10 / 100.0;
+        }
+        if (points % 2 == 0) {
+            bonus += 1;
+        } else {
+            if (points % 10 == 5) {
+                bonus += 2;
+            }
+        }
+
+        System.out.println(bonus);
+        System.out.println(points+bonus);
+    }
+}
+
