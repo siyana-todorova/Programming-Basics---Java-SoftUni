214936206d471366f70fa4e6be834843b4be04c8
SiyanaTodorova <47076923+SiyanaTodorova@users.noreply.github.com>
2019-01-27 12:45:58 +0200

    Delete Expression.java

--- Expression.java
+++
@@ -1,8 +0,0 @@
-public class Expression {
-    public static void main(String[] args) {
-        double a = (3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3;
-        {
-            System.out.println(a);
-        }
-    }
-}
